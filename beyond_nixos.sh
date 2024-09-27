553ee3d690e3c232d09748ec1ee21cab0b52936d
artur <muslimow9@yandex.ru>
2024-09-27 23:09:14 +0300

    Fixed powershell problem 2
    	modified:   beyond_nixos.sh

--- beyond_nixos.sh
+++ beyond_nixos.sh
@@ -35,7 +35,7 @@
 	##powershell
 		cd /tmp/
 		wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.5/powershell_7.4.5-1.deb_amd64.deb
-		sudo apt install ./powershell_7.4.5-1.deb_amd64.deb
+		sudo dpkg --install --ignore-depends=libicu72 ./powershell_7.4.5-1.deb_amd64.deb
 		cd /
 
         ##powershell modules
